24f28af0b1fb709178e0d51e4ac809dbfb31d8d4
Anastasiya Omak <ios.developer.by@gmail.com>
2024-02-12 10:41:14 +0200

    Init Commit

---
+++ UIKitTeamplate/Album.swift
@@ -0,0 +1,20 @@
+// Album.swift
+// Copyright © RoadMap. All rights reserved.
+
+import Foundation
+
+/// модель для хранения инфомации о песне
+struct Album {
+    // MARK: - Properties
+
+    // название песни для воспроизведения
+    var song: String
+    // изображение исполнителя
+    var image: String
+    // название трека
+    var nameSong: String
+    // имя исполнителя
+    var nameArtist: String
+    // длительность песни
+    var time: String
+}
