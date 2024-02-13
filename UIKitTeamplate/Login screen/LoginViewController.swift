54f24e5f7388d04dcbe94e330145815b65bca284
Anastasiya Omak <ios.developer.by@gmail.com>
2024-02-13 22:07:45 +0200

    Add comments

--- UIKitTeamplate/Login screen/LoginViewController.swift
+++ UIKitTeamplate/Login screen/LoginViewController.swift
@@ -6,7 +6,7 @@ import UIKit
 /// Класс отвечает за авторизацию пользователя при входе в приложение
 final class LoginViewController: UIViewController {
     // MARK: - Private Properties
-
+    // Cвойство для создания белого View
     private lazy var whitePartOfScreenView: UIView = {
         let element = UIView()
         element.frame = CGRect(x: 0, y: 248, width: 375, height: 564)
@@ -14,14 +14,14 @@ final class LoginViewController: UIViewController {
         element.layer.cornerRadius = 20
         return element
     }()
-
+    // Cвойство для создания логотипа кафе
     private lazy var cafeLogoImageView: UIImageView = {
         let element = UIImageView()
         element.frame = CGRect(x: 100, y: 75, width: 175, height: 76)
         element.image = UIImage(named: "cafeLogo")
         return element
     }()
-
+    // Cвойство для создания лейбла авторизации
     private lazy var authorizationLabel: UILabel = {
         let element = UILabel()
         element.text = "Авторизация"
@@ -30,7 +30,7 @@ final class LoginViewController: UIViewController {
         element.frame = CGRect(x: 20, y: 32, width: 195, height: 31)
         return element
     }()
-
+    // Cвойство для создания лейбла для логина
     private lazy var loginLabel: UILabel = {
         let element = UILabel()
         element.text = "Логин"
@@ -39,7 +39,7 @@ final class LoginViewController: UIViewController {
         element.frame = CGRect(x: 20, y: 84, width: 175, height: 19)
         return element
     }()
-
+    // Cвойство для создания лейбла для пароля
     private lazy var passwordLabel: UILabel = {
         let element = UILabel()
         element.text = "Пароль"
@@ -48,7 +48,7 @@ final class LoginViewController: UIViewController {
         element.frame = CGRect(x: 20, y: 159, width: 175, height: 19)
         return element
     }()
-
+    // Кнопка для смены отобраения пароля
     private lazy var hidenEyeButton: UIButton = {
         let element = UIButton()
         element.setImage(UIImage(named: "vector"), for: .normal)
@@ -56,7 +56,7 @@ final class LoginViewController: UIViewController {
         element.addTarget(self, action: #selector(hideButtonPressed), for: .touchUpInside)
         return element
     }()
-
+    // TextField для ввода пароля
     private lazy var passwordTextField: UITextField = {
         let element = UITextField()
         element.placeholder = "Введите пароль"
@@ -65,7 +65,7 @@ final class LoginViewController: UIViewController {
         element.frame = CGRect(x: 20, y: 188, width: 129, height: 17)
         return element
     }()
-
+    // TextField для ввода почты
     private lazy var emailTextField: UITextField = {
         let element = UITextField()
         element.placeholder = "Введите почту"
@@ -73,7 +73,7 @@ final class LoginViewController: UIViewController {
         element.frame = CGRect(x: 20, y: 113, width: 175, height: 17)
         return element
     }()
-
+    // Кнопка для открытия меню
     private lazy var loginButton: UIButton = {
         let element = UIButton(type: .system)
         element.titleLabel?.font = UIFont(name: "Verdana-Bold", size: 16)
@@ -87,23 +87,23 @@ final class LoginViewController: UIViewController {
         element.addTarget(self, action: #selector(loginButtonPressed), for: .touchUpInside)
         return element
     }()
-
+    // View для разделения элементов
     private lazy var emailGreyLineView: UIView = {
         let element = UIView()
         element.frame = CGRect(x: 20, y: 138, width: 335, height: 1)
         element.backgroundColor = .gray
         return element
     }()
-
+    // View для разделения элементов
     private lazy var passwordGreyLineView: UIView = {
         let element = UIView()
         element.frame = CGRect(x: 20, y: 214, width: 335, height: 1)
         element.backgroundColor = .gray
         return element
     }()
-
+    
     // MARK: - Life cycle
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupViews()
@@ -111,9 +111,9 @@ final class LoginViewController: UIViewController {
         emailTextField.delegate = self
         passwordTextField.delegate = self
     }
-
+    
     // MARK: - Private Methods
-
+    // Добавление вью на экран
     private func setupViews() {
         view.addSubview(whitePartOfScreenView)
         view.addSubview(cafeLogoImageView)
@@ -127,7 +127,7 @@ final class LoginViewController: UIViewController {
         whitePartOfScreenView.addSubview(emailGreyLineView)
         whitePartOfScreenView.addSubview(passwordGreyLineView)
     }
-
+    // Метод для смены изображения отображающего пароль
     @objc private func hideButtonPressed() {
         if hidenEyeButton.currentImage == UIImage(named: "vector") {
             hidenEyeButton.setImage(UIImage(named: "vectorOpen"), for: .normal)
@@ -137,7 +137,8 @@ final class LoginViewController: UIViewController {
             passwordTextField.isSecureTextEntry = true
         }
     }
-
+    
+    // Метод отработки нажатия на кнопку
     @objc private func loginButtonPressed() {
         let menuViewController = MenuViewController()
         menuViewController.modalPresentationStyle = .fullScreen
@@ -146,7 +147,7 @@ final class LoginViewController: UIViewController {
 }
 
 // MARK: - Extension UITextFieldDelegate
-
+// Расширение для проверки условий для ввода пароля и логина при котором кнопка логина активна или нет
 extension LoginViewController: UITextFieldDelegate {
     func textField(
         _ textField: UITextField,
@@ -159,7 +160,7 @@ extension LoginViewController: UITextFieldDelegate {
             loginButton.isEnabled = true
             loginButton.alpha = 1
         }
-
+        
         return true
     }
 }
